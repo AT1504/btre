eef55fe564a5c827aadd8923eec6dea222be4506
arieltev <arotev@gmail.com>
2019-11-24 21:06:43 +0200

    Section 9 Lec 59

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Contact
+from django.core.mail import send_mail
 
 def contact(request):
     if request.method == 'POST':
@@ -12,10 +13,28 @@ def contact(request):
         message = request.POST['message']
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
+        
+        #check if the user has made inquiry already
+        if request.user.is_authenticated:
+            user_id=request.user.id
+            has_contacted=Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry for this already')
+                return redirect('/listings/'+listing_id)
 
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
+
+        #send email
+        send_mail(
+            'Property listing inquiry',
+            'there has benn an inquiry for'+ listing + '. sign into admin',
+            '',
+            [''],
+            fail_silently=False,
+        )
+
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
 
